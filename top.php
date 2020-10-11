a6991012a6aed95790f401ea818acc5d3744b629
BillyA287 <48728966+BillyA287@users.noreply.github.com>
2020-10-11 23:28:51 +0100

    Add files via upload

---
+++ top.php
@@ -0,0 +1,18 @@
+<?php
+
+
+switch ($page){
+    case 'test.php';
+    include 'top.php';
+    break;
+    case 'middle';
+    include 'middle.php';
+break; 
+case 'bottom';
+include 'bottom.php';
+default: 
+case 'test.php';
+
+}
+
+?>
